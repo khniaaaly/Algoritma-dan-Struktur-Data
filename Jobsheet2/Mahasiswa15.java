284ff4907466e620b27a381dd411a75ae3654598
Khania Puji Auliya <khaniapujia@gmail.com>
2026-02-22 21:18:42 +0700

    Percobaan 1

---
+++ Jobsheet2/Mahasiswa15.java
@@ -0,0 +1,36 @@
+package Jobsheet2;
+
+public class Mahasiswa15 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+
+}
