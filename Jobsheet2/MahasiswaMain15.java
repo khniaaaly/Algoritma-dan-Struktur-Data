ee9c947ece6aba31866bb523ad313b30508a9a13
Khania Puji Auliya <khaniapujia@gmail.com>
2026-02-22 21:49:27 +0700

    Percobaan 3

--- Jobsheet2/MahasiswaMain15.java
+++ Jobsheet2/MahasiswaMain15.java
@@ -2,6 +2,8 @@ package Jobsheet2;
 
 public class MahasiswaMain15 {
     public static void main(String[] args) {
+
+        // objek konstruktor default
         Mahasiswa mhs1 = new Mahasiswa();
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
@@ -9,9 +11,14 @@ public class MahasiswaMain15 {
         mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
-        mhs1.ubahKelas("SI 2J");
+        mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        // objek konstruktor parameter
+        Mahasiswa mhs2 = new Mahasiswa("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
 
@@ -21,11 +28,24 @@ class Mahasiswa {
     String kelas;
     double ipk;
 
+    // konstruktor default
+    Mahasiswa() {
+
+    }
+
+    // kontruktor parameter
+    Mahasiswa(String nama, String nim, double ipk, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.ipk = ipk;
+        this.kelas = kelas;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama  : " + nama);
         System.out.println("NIM   : " + nim);
-        System.out.println("Kelas : " + kelas);
         System.out.println("IPK   : " + ipk);
+        System.out.println("Kelas : " + kelas);
         System.out.println("-------------------------------");
     }
 
