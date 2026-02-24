bb7e69011a9e784d60442ea324467ebee9dded04
Khania Puji Auliya <khaniapujia@gmail.com>
2026-02-24 10:07:12 +0700

    Percobaan 3.4

---
+++ Jobsheet3/Matakuliah15.java
@@ -0,0 +1,15 @@
+package Jobsheet3;
+
+public class Matakuliah15 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah15(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
