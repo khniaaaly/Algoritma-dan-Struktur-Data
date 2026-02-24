f3e02f5b7fc49390f7f09a5ccf052ecb143eee57
Khania Puji Auliya <khaniapujia@gmail.com>
2026-02-24 10:39:55 +0700

    Modifikasi Percobaan 3.4 no. 3 dan 2

--- Jobsheet3/Matakuliah15.java
+++ Jobsheet3/Matakuliah15.java
@@ -6,10 +6,30 @@ public class Matakuliah15 {
     public int sks;
     public int jumlahJam;
 
+    public Matakuliah15() {
+
+    }
+
     public Matakuliah15(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode       : " + kode);
+        System.out.println("Nama       : " + nama);
+        System.out.println("SKS        : " + sks);
+        System.out.println("Jumlah Jam : " + jumlahJam);
+        System.out.println("----------------------------------");
+
     }
 }
