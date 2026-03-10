72813f2b89bc42b734dd389dfbe35042c819d505
Khania Puji Auliya <khaniapujia@gmail.com>
2026-03-10 09:46:10 +0700

    Percobaan 5.3

---
+++ BruteForceDivideConquer/minggu5/MainPangkat15.java
@@ -0,0 +1,28 @@
+package BruteForceDivideConquer.minggu5;
+
+import java.util.Scanner;
+
+public class MainPangkat15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen: ");
+        int elemen = input.nextInt();
+
+        Pangkat15[] png = new Pangkat15[elemen];
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan nilai basis elemen ke-" + (i + 1) + ": ");
+            int basis = input.nextInt();
+            System.out.print("Masukkan nilai pangkat elemen ke-" + (i + 1) + ": ");
+            int pangkat = input.nextInt();
+            png[i] = new Pangkat15(basis, pangkat);
+        }
+        System.out.println("HASIL PANGKAT BRUTEFORCE: ");
+        for (Pangkat15 p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatBF(p.nilai, p.pangkat));
+        }
+        System.out.println("HASIL PANGKAT DIVIDE AND CONQUER: ");
+        for (Pangkat15 p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatDC(p.nilai, p.pangkat));
+        }
+    }
+}
