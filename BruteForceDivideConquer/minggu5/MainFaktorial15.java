f58008ab5eaa8ecfb3448dec9351c9d4de0d30da
Khania Puji Auliya <khaniapujia@gmail.com>
2026-03-10 09:45:39 +0700

    Percobaan 5.2

---
+++ BruteForceDivideConquer/minggu5/MainFaktorial15.java
@@ -0,0 +1,15 @@
+package BruteForceDivideConquer.minggu5;
+
+import java.util.Scanner;
+
+public class MainFaktorial15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai: ");
+        int nilai = input.nextInt();
+
+        Faktorial15 fk = new Faktorial15();
+        System.out.println("Nilai faktorial " + nilai + " menggunakan BF: " + fk.faktorialBF(nilai));
+        System.out.println("Nilai faktorial " + nilai + " menggunakan DC: " + fk.faktorialDC(nilai));
+    }
+}
