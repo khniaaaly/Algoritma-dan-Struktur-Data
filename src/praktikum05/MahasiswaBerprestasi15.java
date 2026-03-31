bc1445847481cf473fe4eecfd03c09f997531952
Khania Puji Auliya <khaniapujia@gmail.com>
2026-03-31 10:00:30 +0700

    Revisi Praktikum 4

--- src/praktikum05/MahasiswaBerprestasi15.java
+++ src/praktikum05/MahasiswaBerprestasi15.java
@@ -47,7 +47,7 @@ public class MahasiswaBerprestasi15 {
     void insertionSort() {
         for (int i = 1; i < listMhs.length; i ++) {
             Mahasiswa15 temp = listMhs[i];
-            int j = 1;
+            int j = i;
             while (j > 0 && listMhs[j-1].ipk > temp.ipk) {
                 listMhs[j] = listMhs[j-1];
                 j--;
