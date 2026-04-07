60ccee16cc57b98a9a1a25c1d6811453b1e2b5eb
Khania Puji Auliya <khaniapujia@gmail.com>
2026-04-07 07:30:59 +0700

    Percobaan 1

--- src/praktikum05/MahasiswaDemo15.java
+++ src/praktikum05/MahasiswaDemo15.java
@@ -17,11 +17,26 @@ public class MahasiswaDemo15 {
             System.out.print("IPK   : ");
             double ipk = sc.nextDouble();
             sc.nextLine();
+            System.out.println("-------------------------------");
 
             Mahasiswa15 m = new Mahasiswa15(nim, nama, kelas, ipk);
             list.tambah(m);
-            System.out.println();
         }
+
+        list.tampil();
+        //melakukan pencarian data sequential
+        System.out.println("---------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("---------------------------------------------");
+        System.out.println("Masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = sc.nextDouble();
+
+        System.out.println("menggunakan sequential searching");
+        double posisi = list.sequentialSearching(cari);
+        int pss = (int) posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
 
         System.out.println("Data Mahasiswa sebelum sorting: ");
         list.tampil();
