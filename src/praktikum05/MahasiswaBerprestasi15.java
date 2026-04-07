abe77c4050885bd441609b2153266f8d0fac3bf0
Khania Puji Auliya <khaniapujia@gmail.com>
2026-04-07 08:35:30 +0700

    Modifikasi 2 pertanyaan 5

--- src/praktikum05/MahasiswaBerprestasi15.java
+++ src/praktikum05/MahasiswaBerprestasi15.java
@@ -93,7 +93,7 @@ public class MahasiswaBerprestasi15 {
             if (cari == listMhs[mid].ipk) {
                 return (mid);
             } 
-            else if (listMhs[mid].ipk>cari) {
+            else if (listMhs[mid].ipk<cari) {
                 return findBinarySearch(cari, left, mid-1);
             } else {
                 return findBinarySearch(cari, mid+1, right);
