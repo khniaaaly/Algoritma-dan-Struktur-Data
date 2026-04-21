ef02ddd080a2a0a34dfe3c5dcf0485075eb122c4
Khania Puji Auliya <khaniapujia@gmail.com>
2026-04-21 08:23:45 +0700

    Percobaan 1

---
+++ src/Jobsheet09/MahasiswaS15.java
@@ -0,0 +1,20 @@
+public class MahasiswaS15 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    MahasiswaS15(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+    
+    
+}
+
