38efde2692008565703c0a9c9fe3defd83b5208d
Khania Puji Auliya <khaniapujia@gmail.com>
2026-04-21 08:51:02 +0700

    Modifikasi Percobaan 1

--- src/Jobsheet09/StackTugasMahasiswa15.java
+++ src/Jobsheet09/StackTugasMahasiswa15.java
@@ -55,9 +55,21 @@ public class StackTugasMahasiswa15 {
     }
 
     public void print() {
-        for (int i = 0; i <= top; i++) {
+        for (int i = top; i >= 0; i--) {
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
         }
-        System.out.println("");
+    }
+
+    public MahasiswaS15 peekBottom() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong!");
+            return null;
+        }
+    }
+
+    public int jumlahTugas() {
+        return top + 1;
     }
 }
