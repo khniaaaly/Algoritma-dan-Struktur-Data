1156ac7ea305937e44027ae224b3a8efc0f75db3
Khania Puji Auliya <khaniapujia@gmail.com>
2026-05-05 08:29:00 +0700

    Modifikasi Percobaan 1

--- src/Pertemuan11/SLLMain15.java
+++ src/Pertemuan11/SLLMain15.java
@@ -1,19 +1,28 @@
+import java.util.Scanner;
 public class SLLMain15 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList15 sll = new SingleLinkedList15();
 
-        MahasiswaL15 mhs1 = new MahasiswaL15("24212200", "Alvaro", "1A", 4.0);
-        MahasiswaL15 mhs2 = new MahasiswaL15("23212201", "Bimon", "2B", 3.8);
-        MahasiswaL15 mhs3 = new MahasiswaL15("22212202", "Cintia", "3C", 3.5);
-        MahasiswaL15 mhs4 = new MahasiswaL15("21212203", "Dirga", "4D", 3.6);
+        System.out.print("Jumlah data mahasiswa: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nData Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+            MahasiswaL15 mhs = new MahasiswaL15(nim, nama, kelas, ipk);
+            sll.addLast(mhs);
+        }
+        System.out.println("\nData Linked List:");
         sll.print();
     }
 }
