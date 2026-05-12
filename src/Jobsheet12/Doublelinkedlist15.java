f849e133460f4db8bd874287432da354f6058122
Khania Puji Auliya <khaniapujia@gmail.com>
2026-05-12 08:30:50 +0700

    Percobaan 1

---
+++ src/Jobsheet12/Doublelinkedlist15.java
@@ -0,0 +1,66 @@
+public class Doublelinkedlist15 {
+    NodeDDL15 head;
+    NodeDDL15 tail;
+
+    public Doublelinkedlist15() {
+        head = null;
+        tail = null;
+    }
+    public boolean isEmpty() {
+        return head == null;
+    }
+    public void addFirst(MahasiswaDDL15 data) {
+        NodeDDL15 newNode = new NodeDDL15(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+    public void addLast(MahasiswaDDL15 data) {
+        NodeDDL15 newNode = new NodeDDL15(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+    public void insertAfter (String keyNim, MahasiswaDDL15 data) {
+        NodeDDL15 current = head;
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+        if (current == null) {
+            System.out.println("Data dengan NIM " + keyNim + " tidak ditemukan.");
+            return;
+        }
+        NodeDDL15 newNode = new NodeDDL15(data);
+
+        if(current == tail) {
+            newNode.prev = current;
+            current.next = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = current;
+            newNode.next = current.next;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+        System.out.println("Data berhasil disisipkan setelah NIM " + keyNim);
+    }
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Linked List maasih kosong.");
+            return;
+        }
+        NodeDDL15 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+}
---
+++ src/Jobsheet12/NodeDDL15.java
@@ -0,0 +1,11 @@
+public class NodeDDL15 {
+    MahasiswaDDL15 data;
+    NodeDDL15 prev;
+    NodeDDL15 next;
+
+    public NodeDDL15(MahasiswaDDL15 data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
